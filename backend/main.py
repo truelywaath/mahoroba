3352b01245c53274741dff7e599780b5c32b3c76
aosanBusiness <aosan.business@gmail.com>
2023-08-07 01:56:07 +0900

    main.pyにapp=とapp.runを追記

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from flask import Flask, request
 
+app = Flask(__name__)
+
 @app.route('/', methods=['POST'])
 def index():
     # フロントエンドから受信したデータ
     data = request.json.get('name') 
-    return {"greeting": "Hello! {}".format(name)}
+    return {"greeting": "Hello! {}".format(data)}
+
+app.run()
