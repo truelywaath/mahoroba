d6e2a1f428c063a558a50fdce7b74cfab8599a05
truelywaath <fuel.owy@gmail.com>
2023-08-10 11:08:56 +0900

    connect front and back

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,10 @@
 from flask import Flask
-from flask import request, make_response, jsonify
 from flask_cors import CORS
+from views import views
 
 app = Flask(__name__, static_folder="./build/static", template_folder="./build")
+app.register_blueprint(views)
 CORS(app) #Cross Origin Resource Sharing
-
-@app.route("/", methods=['GET'])
-def index():
-    return "text parser:)"
-
-@app.route("/sample1", methods=['GET','POST'])
-def parse():
-    data = request.get_json()
-    text = data['post_text']
-    res = text + text
-
-    response = {'result': res}
-    return make_response(jsonify(response))
 
 if __name__ == "__main__":
     app.debug = True
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,60 @@
+import React from 'react';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around items-center">
-      <h1 className="text-3xl font-bold underline">
-        Hello Mahoroba!
-      </h1>
-      <p className="m-0 text-gray-400">mahoroba</p>
-      <button className="bg-gray-300 border-0 p-2 rounded-md hover:bg-gray-400 hover:text-white">ボタン</button>
-    </div>
-  );
+//function App() {
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around items-center">
+        <h1 className="text-3xl font-bold underline">
+          Hello Mahoroba!
+        </h1>
+        <p className="m-0 text-gray-400">mahoroba</p>
+        <button className="bg-gray-300 border-0 p-2 rounded-md hover:bg-gray-400 hover:text-white">ボタン</button>
+      </div>
+        <header className="App-header">
+          <h1>text * 2</h1>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              <textarea name="text" cols="80" rows="4" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <br/>
+            <input type="submit" value="doublify!" />
+          </form>
+        </header>
+      </div>
+    );
+  }
+
+
+  doubled_text = text => {
+    //console.log("input text >>"+text)
+    Axios.post('http://127.0.0.1:5000/doublify', {
+      post_text: text
+    }).then(function(res) {
+      alert(res.data.result);
+    })
+  };
+
+  handleSubmit = event => {
+    this.doubled_text(this.state.value)
+    event.preventDefault();
+  };
+
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
 }
 
 export default App;
+
