cb4eabe563b5d75aea0289eb96794323709be08b
truelywaath <fuel.owy@gmail.com>
2023-08-10 17:29:17 +0900

    delete unnecessary line

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,7 +1,5 @@
 import pandas as pd
-
 from main import db
-from main.models import User, ProfessionalCourse, SpecificCourse, GeneralCourse, License
 
 db.drop_all()
 db.create_all()
--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,2 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
-
-from main import db, login_manager
