85d73bd4ff969f625c4430a2fdfd1cd7db62f8d2
aosanBusiness <aosan.business@gmail.com>
2023-08-31 23:31:45 +0900

    fix SelectTimezone kan

--- frontend/src/components/SelectTimezone.jsx
+++ frontend/src/components/SelectTimezone.jsx
@@ -21,13 +21,13 @@ export function SelectTimezone() {
   return(
     <>
       <Header /> 
-      <div className="bg-rose-50 w-full text-center text-white text-7xl py-12 mb-10">
+      <div className="bg-rose-50 w-full text-center text-white text-[60px] font-bold pt-6 pb-9">
         時間帯を選択してください
       </div>
-      <div className="px-8">
+      <div className="mx-12 mt-9">
         <Link to="/area">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-20 h-20 text-rose-300">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-[75px] h-[75px]">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
           </svg>
         </Link>
         <div className="text-center">
@@ -35,9 +35,9 @@ export function SelectTimezone() {
             {timezones.map((timezone) => {
               return (
                 <div className="list-row">
-                  <li className="my-4" key={timezone.id}>
+                  <li className="mb-12" key={timezone.id}>
                     <Link to="/spot" state={{ timezone_id: timezone.id, area_id: area_id }}>
-                      <button className="w-2/3 text-5xl text-rose-400 bg-rose-200 hover:bg-rose-300 py-4 my-2 px-4 border border-gray-400 rounded shadow rounded-full" component={Link} to="/">
+                      <button className="w-[516px] h-[96px] text-4xl font-semibold text-white bg-rose-200 hover:text-rose-600 hover:bg-rose-300  border border-gray-400 rounded shadow rounded-full" component={Link} to="/">
                         { timezone.timezone }
                       </button>
                     </Link>
@@ -46,10 +46,10 @@ export function SelectTimezone() {
               )
             })}
             <div className="list-row">
-              <li className="my-4" key="-1">
+              <li className="mb-[794px]" key="-1">
                 <Link to="/spot" state={{ timezone_id: -1, area_id: area_id }}>
-                  <button className="w-2/3 text-5xl text-rose-400 bg-rose-200 hover:bg-gray-300 py-4 my-2 px-4 border border-gray-400 rounded shadow rounded-full" component={Link} to="/">
-                   おまかせ
+                  <button className="w-[516px] h-[96px] text-4xl font-semibold text-white bg-rose-200 hover:text-rose-600 hover:bg-rose-300  border border-gray-400 rounded shadow rounded-full" component={Link} to="/">
+                   指定なし
                   </button>
                 </Link>
               </li>
