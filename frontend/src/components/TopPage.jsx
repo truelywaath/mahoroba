a2eda4c43b219f708a1378bc2b1e194e9948cdfe
aosanBusiness <aosan.business@gmail.com>
2023-08-31 22:26:37 +0900

    fix TopPage kan

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -5,27 +5,31 @@ import './../App.css';
 const style = {
   width: "auto",
   backgroundImage: `url(images/toppage.jpg)`,
-  backgroundSize: "cover",
+  backgroundSize: "contain",
  };
 
 export function TopPage() {
   return(
     <>
     <div style={ style }>
-      <div className="bg-black bg-opacity-30">
+      <div className="bg-black bg-opacity-10">
       <div className="text-center">
-        <div className="py-8"></div>
-          <h1 className="text-7xl font-bold text-white pt-96"> エドまちライブラリ </h1>
-          <h1 className="text-5xl text-white pt-20 pb-96"> 画面からあなたが本当に行きたい場所へ </h1>
+        <div className="pt-[614px]"></div>
+        <div className="py-[40px]">
+          <div className="py-32 bg-rose-0/30">
+            <p className="text-8xl font-extrabold text-white"> エドまちライブラリ </p>
+            <p className="text-[40px] font-semibold text-white "> 画像からあなたが本当に行きたい場所へ </p>
+          </div>
+        </div>
           <div className="py-32"></div>
-          <div className="bg-rose-800 py-32">
+          <div className="bg-rose-800 py-20 font-normal">
             <p className="text-3xl">エドまちライブラリとは</p>
             <p className="text-3xl">東京都内のあなたにとって素敵な場所との出会いをお手伝いします。</p>
             <p className="text-3xl">訪れるエリアと時間帯から、気になる情景を探索してみましょう</p>
           <ul>
-              <li className="mt-20">
-                <Link className="bg-rose-800 border-4 border-rose text-rose font-bold p-6 rounded-full text-5xl px-32" to="/area">
-                  start
+              <li className="mt-16">
+                <Link className="bg-rose-800 border-8 border-rose text-rose font-bold py-2 rounded-full text-5xl px-64" to="/area">
+                  Start
                 </Link>
               </li>
             </ul>
